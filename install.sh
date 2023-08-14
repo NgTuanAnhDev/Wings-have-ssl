442f5c931b80ad84374db88a608609b7b0c4f4a6
NgTuanAnhDev <142076097+NgTuanAnhDev@users.noreply.github.com>
2023-08-15 00:01:06 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,25 @@ chmod u+x /usr/local/bin/wings
 
 echo "Cấu hình Wings..."
 
-cp wings.service /etc/systemd/system/wings.service
+echo "[Unit]
+Description=Pterodactyl Wings Daemon
+After=docker.service
+Requires=docker.service
+PartOf=docker.service
+
+[Service]
+User=root
+WorkingDirectory=/etc/pterodactyl
+LimitNOFILE=4096
+PIDFile=/var/run/wings/daemon.pid
+ExecStart=/usr/local/bin/wings
+Restart=on-failure
+StartLimitInterval=180
+StartLimitBurst=30
+RestartSec=5s
+
+[Install]
+WantedBy=multi-user.target" | sudo tee /etc/systemd/system/wings.service
 read -p "Nhập tên miền của bạn (ví dụ: example.com): " domain
 read -p "Nhập STT Node: " node
 
